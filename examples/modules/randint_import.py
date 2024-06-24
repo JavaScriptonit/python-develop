eeb8adbbb1be615789fd72002e57492e1cd4c0dc
Andrey <shabunovaa@yandex.ru>
2024-06-24 19:17:22 +0300

    add modules lesson 7 p2

---
+++ examples/modules/randint_import.py
@@ -0,0 +1,8 @@
+
+from random import randint
+import random # Импортируем функцию randint из модуля random
+# from random import * # импортировать всё из модуля random
+
+result = randint(1, 100)  # Генерируем случайное целое число в диапазоне от 1 до 100 и сохраняем его в переменную result
+
+print(result)  # Выводим сгенерированное случайное число
